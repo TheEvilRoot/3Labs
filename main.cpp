3315a2c70758cd56c297ed4fcc727131e4846858
Master Quoter Branch <robot-creatix@ya.ru>
2019-09-14 05:57:02 +0000

    Initial

---
+++ main.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <limits>
+
+const int ERR_SUCCEED = 0;
+const int ERR_INVALID_VALUE = 1;
+const int ERR_LOWER_BORDER_EXCEED = 2;
+const int ERR_UPPER_BORDER_EXCEED = 3;
+
+const char * localizeError(char error) {
+  switch (error) {
+    case ERR_SUCCEED:
+      return "Succeed";
+    case ERR_INVALID_VALUE:
+      return "Invalid value";
+    case ERR_UPPER_BORDER_EXCEED:
+      return "Upper border exceeded";
+    case ERR_LOWER_BORDER_EXCEED:
+      return "Lower border exceeded";
+    default:
+      return "Unexpected error";
+  }
+}
+
+template <typename T>
+std::pair<char, T> enter(const char *message, T lower, T upper) {
+  T value;
+  char error = ERR_SUCCEED;
+  std::cout << message;
+  auto isFailed = !(std::cin >> value);
+
+  if (isFailed) {
+    if (std::cin.fail()) {
+      std::cin.clear();
+    }
+    error = ERR_INVALID_VALUE;
+  } else {
+    if (value < lower) {
+      error = ERR_LOWER_BORDER_EXCEED;
+    }
+    if (value > upper) {
+      error = ERR_UPPER_BORDER_EXCEED;
+    }
+  }
+  std::cin.ignore(std::numeric_limits<int>::max(), '\n');
+
+  return std::pair<char, T>(error, value);
+}
+
+template <typename T>
+T handleInput(const char *message, T lower, T upper, const char * (*localize)(char) = &localizeError) {
+  std::pair<char, T> res;
+
+  while (true) {
+    res = enter(message, lower, upper);
+    if (res.first != ERR_SUCCEED) {
+      std::cout << "[EE] (" << res.first << ") " << localize(res.first) << "\n";
+    } else break;
+  }
+  return res.second;
+}
+
+std::string enterString(const char *message, bool includeWhitespaces = true) {
+  std::string string;
+  char c;
+
+  std::cout << message;
+  while ((c = std::cin.get())) {
+    if (c == '\n') break;
+    if (!includeWhitespaces && c == ' ') {
+      std::cin.ignore(std::numeric_limits<int>::max(), '\n');
+      break;
+    }
+    string += c;
+  }
+
+  return string;
+}
+
+int main() {
+ return 0;
+}
+
