b31842f970d657d01d03155c169f70db90f695a0
Master Quoter Branch <robot-creatix@ya.ru>
2019-09-15 08:42:56 +0000

    Test

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
+#include <cstdlib>
 #include <iostream>
 #include <limits>
+#include <stdexcept>
 
 const int ERR_SUCCEED = 0;
 const int ERR_INVALID_VALUE = 1;
@@ -76,7 +78,83 @@ std::string enterString(const char *message, bool includeWhitespaces = true) {
   return string;
 }
 
+class MyArray {
+public:
+    static int GLOBAL_ID;
+    int id;
+    char *pointer;
+    int length;
+
+    MyArray() {
+        this->id = GLOBAL_ID++;
+        std::cout << "[DB] MyArray " << id << " initialization\n";
+        this->pointer = NULL;
+        this->length = 0;
+    }
+
+    ~MyArray() {
+        std::cout << "[DB] MyArray " << id << " deinitialization\n";
+        delete[] this->pointer;
+    }
+
+    void push(char data) {
+        std::cout << "[DB] MyArray " << id << " pushing " << data << "\n";
+        this->pointer = (char*) realloc(this->pointer, ++(this->length));
+        this->pointer[length - 1] = data;
+    }
+
+    char & operator[](int index) {
+        std::cout << "[DB] MyArray " << id << " getting " << index << "\n";
+         if (index < 0 || index >= length) {
+             throw std::out_of_range("");
+         }
+         return pointer[index];
+      }
+
+    MyArray intersections(MyArray &with) {
+        MyArray newArr;
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < with.length; j++) {
+                if (pointer[i] == with[j]) {
+                    newArr.push(pointer[i]);
+                }
+            }
+        }
+        return newArr;
+    }
+
+    friend std::ostream & operator << (std::ostream &out, const MyArray &arr);
+
+};
+
+int MyArray::GLOBAL_ID = 0;
+
+std::ostream & operator << (std::ostream &out, const MyArray &arr)
+{
+    for (int i = 0; i < arr.length; i++) {
+        std::cout << arr.pointer[i] << " ";
+    }
+    return out;
+}
+
+
 int main() {
- return 0;
+    MyArray arr;
+    arr.push('a');
+    arr.push('b');
+
+    std::cout << "ARR: " << arr << "\n";
+
+    MyArray brr;
+    brr.push('b');
+    brr.push('c');
+
+    std::cout << "BRR: " << brr << "\n";
+
+    MyArray crr = arr.intersections(brr);
+
+    std::cout << "CRR: " << crr << "\n";
+
+    return 0;
 }
 
