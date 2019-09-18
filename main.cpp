f26f20ce636c1be6c85ac4d1f0db083ba36c61e1
Master Quoter Branch <robot-creatix@ya.ru>
2019-09-18 06:04:38 +0000

    indev

--- main.cpp
+++ main.cpp
@@ -1,18 +1,24 @@
 #include <iostream>
 #include <limits>
+#include <ncurses>
 
 #include "complex_number.hpp"
 #include "input.hpp"
 
 ComplexNumber enterComplexNumber() {
-    float r = handleInput("Enter real: ", -std::numeric_limits<float>::max(), std::numeric_limits<float>::max());
-    float i = handleInput("Enter imaginary: ", -std::numeric_limits<float>::max(), std::numeric_limits<float>::max());
-    ComplexNumber ret(r, i);
+    float r = handleInput("Enter real: ", -std::numeric_limits<float>::max(),
+        std::numeric_limits<float>::max());
+    float i = handleInput("Enter imaginary: ", -std::numeric_limits<float>::max(),
+        std::numeric_limits<float>::max());
 
+    ComplexNumber ret(r, i);
     return ret;
 }
 
 int main() {
+
+    getmaxyx(stdscr, 100, 100);
+
     ComplexNumber num(enterComplexNumber());
     ComplexNumber abc(enterComplexNumber());
 
