d2550ebdda09dac02c0f7eea0666bbc548d1c776
Master Quoter Branch <robot-creatix@ya.ru>
2019-10-21 08:52:32 +0000

    SOme fix

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@ const int ERR_UPPER_BORDER_EXCEED = 4;
 
 const int ERR_INVALID_ARGUMENTS = 5;
 const int ERR_NO_SUCH_CMD = 6;
+const int ERR_NO_VECTORS = 7;
 
 template <typename T>
 std::pair<char, T> enter(const char *message, T lower, T upper) {
@@ -82,7 +83,7 @@ void sort(T *array, size_t size, int (*comparator)(T*,T*)) {
 struct Vector {
   int x;
   int y;
-  
+
   float mag() {
     return sqrt(x * x + y * y);
   }
@@ -112,7 +113,7 @@ std::string trim(const std::string& str)
 {
   size_t first = str.find_first_not_of(' ');
   if (std::string::npos == first) return str;
-  
+
   size_t last = str.find_last_not_of(' ');
   return str.substr(first, (last - first + 1));
 }
@@ -139,6 +140,7 @@ void setupI18n(std::map<const int, std::string>& i18n) {
   i18n.insert(std::pair<const int, std::string>(ERR_UPPER_BORDER_EXCEED, "tesh: Value should be lower"));
   i18n.insert(std::pair<const int, std::string>(ERR_NO_SUCH_CMD, "tesh: Command not found"));
   i18n.insert(std::pair<const int, std::string>(ERR_INVALID_ARGUMENTS, "tesh: Command arguments is invalid. Enter help to get manual"));
+  i18n.insert(std::pair<const int, std::string>(ERR_NO_VECTORS, "tesh: Vector list is empty"));
 }
 
 int helpCmd(std::vector<std::string> &args, std::vector<Vector>& vectors) {
@@ -147,7 +149,7 @@ int helpCmd(std::vector<std::string> &args, std::vector<Vector>& vectors) {
   std::cout << "\tadd <x> <y> - add new vector to the list. Accepts two parameters: x(number), y(number)\n";
   std::cout << "\tprint (index) - print vectors array. If index is passed, prints only selected vector\n";
   std::cout << "\texit - exit session\n";
-  
+
   return 0;
 }
 
@@ -163,19 +165,22 @@ int addCmd(std::vector<std::string> &args, std::vector<Vector>& vectors) {
   } catch(size_t idx) {
     return ERR_INVALID_VALUE;
   }
-  
+
   vectors.push_back({x, y});
   return 0;
 }
 
 int printCmd(std::vector<std::string> &args, std::vector<Vector>& vectors) {
+  if (vectors.size() == 0) {
+    return ERR_NO_VECTORS;
+  }
   int index = 0;
   try {
     if (args.size() < 2) throw 1;
     index = parseInt(args[1]);
     if (index < 0) index = vectors.size() + index;
     if (index >= vectors.size()) throw 2;
-    
+
     auto vec = vectors[index];
     std::cout << "(" << vec.x << ", " << vec.y << ") = " << vec.mag() << "\n";
   } catch(...) {
@@ -187,6 +192,9 @@ int printCmd(std::vector<std::string> &args, std::vector<Vector>& vectors) {
 }
 
 int sortCmd(std::vector<std::string> &args, std::vector<Vector>& vectors) {
+  if (vectors.size() == 0) {
+    return ERR_NO_VECTORS;
+  }
   sort(vectors.data(), vectors.size(), &vectorCompare);
   return 0;
 }
@@ -200,35 +208,35 @@ typedef int(*Callback)(std::vector<std::string>&, std::vector<Vector>&);
 int main() {
   std::map<const int, std::string> i18n;
   setupI18n(i18n);
-  
+
   std::map<std::string, Callback> commands;
   commands.insert(std::pair<std::string, Callback>("help", &helpCmd));
   commands.insert(std::pair<std::string, Callback>("add", &addCmd));
   commands.insert(std::pair<std::string, Callback>("print", &printCmd));
   commands.insert(std::pair<std::string, Callback>("sort", &sortCmd));
   commands.insert(std::pair<std::string, Callback>("exit", &exitCmd));
-  
+
   std::vector<Vector> vectors;
   std::cout << "Welcome to theevilshell\n";
   while (1) {
     auto inputString = enterString("tesh > ");
     auto trimmed = trim(inputString);
     if (trimmed.length() == 0) continue;
-    
+
     auto args = splitString(trimmed);
     if (args.size() < 1) continue;
-    
+
     if (commands.find(args[0]) == commands.end()) {
       auto localizedMessage = i18n[ERR_NO_SUCH_CMD];
       std::cout << localizedMessage << ": " << args[0] << "\n";
       continue;
     }
-    
+
     auto command = commands[args[0]];
     auto exitCode = command(args, vectors);
-    
+
     if (exitCode == 255) break;
-    
+
     if (i18n.find(exitCode) != i18n.end()) {
       std::cout << i18n[exitCode] << "\n";
     } else if (exitCode != 0) {
@@ -237,21 +245,3 @@ int main() {
   }
   return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
