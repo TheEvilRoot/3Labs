8d7a15f02c9e48f4e3c4b7afac994271f26855fe
TheEvilRoot <robot-creatix@ya.ru>
2019-11-10 19:03:32 +0300

    clean

--- main.cpp
+++ main.cpp
@@ -2,18 +2,16 @@
 
 #include <map>
 #include <functional>
-#include <fstream>
 #include <utility>
 #include <string>
 
 #include "api.h"
 
-struct FileContext {
-  std::fstream file;
-  std::string fileName;
+struct Context {
+
 };
 
-typedef std::function<const char(FileContext&, std::vector<std::string>&, InputHandler&)> CommandCallback;
+typedef std::function<const char(Context&, std::vector<std::string>&, InputHandler&)> CommandCallback;
 
 struct Command {
   CommandCallback func;
@@ -27,125 +25,13 @@ void cmd(CommandsMap &commands, const std::string& name, std::string description
     });
 }
 
-bool writeToOutFile(const std::string& content) {
-    std::ofstream file("out.txt", std::ios::out | std::ios::trunc);
-    if (!file.is_open()) {
-        std::cout << "Failed to open output file (out.txt)\n";
-        return false;
-    }
-
-    file << content;
-    file.flush();
-    file.close();
-    return true;
-}
-
 void init(CommandsMap& commands) {
-    cmd(commands, "help", "Display help message", [&](FileContext& context, std::vector<std::string>& args, InputHandler& handler){
+    cmd(commands, "help", "Display help message", [&](Context& context, std::vector<std::string>& args, InputHandler& handler){
         return ERR_SUCCEED;
     });
 
-    cmd(commands, "exit", "Exit the program", [&](FileContext& context, std::vector<std::string>& args, InputHandler& handler) {
+    cmd(commands, "exit", "Exit the program", [&](Context& context, std::vector<std::string>& args, InputHandler& handler) {
         return ERR_EXIT_CODE;
-    });
-
-    cmd(commands, "open", "Open file", [&](FileContext& context, std::vector<std::string>& args, InputHandler& handler) -> char{
-        if (context.file.is_open()) {
-            std::cout << "File " << context.fileName << " already opened. Use 'close' command to close file.\n";
-            return ERR_SUCCEED;
-        }
-
-        if (args.empty()) {
-            std::cout << "You should provide filename to open file\n";
-            std::cout << "open <file>\n";
-            return ERR_INVALID_ARGS;
-        }
-
-        auto fileName = args[0];
-        context.file.open(fileName, std::fstream::in);
-
-        if (!context.file.is_open()) {
-            std::cout << "Failed to open file " << fileName << "\n";
-            return ERR_SUCCEED;
-        }
-
-        std::cout << "File opened!\n";
-        context.file.seekg(0, std::fstream::end);
-        std::cout << "File length: " << context.file.tellg() << "\n";
-        context.file.seekg(0, std::fstream::beg);
-
-        return ERR_SUCCEED;
-    });
-
-    cmd (commands, "close", "Close file", [&](FileContext& context, std::vector<std::string>& args, InputHandler& handler) -> char {
-        if (!context.file.is_open()) {
-            std::cout << "File not opened yet\n";
-            return ERR_SUCCEED;
-        }
-
-        context.file.close();
-        context.fileName = "";
-
-        std::cout << "File closed\n";
-        return ERR_SUCCEED;
-    });
-
-    cmd (commands, "count", "Count specified symbols in file", [&](FileContext& context, std::vector<std::string>& args, InputHandler& handler) -> char {
-        if (!context.file.is_open()) {
-          std::cout << "File not opened yet\n";
-          return ERR_SUCCEED;
-        }
-
-        char charOfInterest = 0;
-        if (args.empty()) {
-            auto str = handler.enterString("Enter symbol to count in file: ");
-            if (str.empty()) return ERR_INVALID_ARGS;
-            charOfInterest = str[0];
-        } else {
-            charOfInterest = args[0][0];
-        }
-        // asserts charOfInterest is not 0
-
-        context.file.clear();
-        context.file.seekg(0, std::fstream::beg);
-        size_t count = 0;
-        while (!context.file.eof()) {
-            int i = 0;
-            if ((i = context.file.get()) < 0) { break; }
-            if (i  == charOfInterest) count++;
-        }
-
-        std::cout << "Symbol '" << charOfInterest << "' appears in file " << count << " times\n";
-
-        if (writeToOutFile(std::to_string(count))) {
-          std::cout << "Result successfully stored to out.txt\n";
-        }
-
-        return ERR_SUCCEED;
-    });
-
-    cmd (commands, "words", "Count words in file", [&](FileContext& context, std::vector<std::string>& args, InputHandler& handler) -> char {
-      if (!context.file.is_open()) {
-          std::cout << "File not opened yet\n";
-          return ERR_SUCCEED;
-      }
-
-      context.file.clear();
-      context.file.seekg(0, std::fstream::beg);
-
-      size_t count = 0;
-      while (!context.file.eof()) {
-          int i = 0;
-          if ((i = context.file.get()) < 0) { break; }
-          if (!((i >= 'a' && i <= 'z') || (i >= 'A' && i <= 'Z') || (i >= '0' &&  i <= '9'))) count++;
-      }
-
-      std::cout << "In file " << count + 1 << " words\n";
-
-      if (writeToOutFile(std::to_string(count + 1))) {
-          std::cout << "Result successfully stored to out.txt\n";
-      }
-      return ERR_SUCCEED;
     });
 
 }
@@ -161,7 +47,7 @@ void init(I18N& i18n) {
     i18n.insert({ ERR_EXIT_CODE, "Succeed" });
 }
 
-char run(FileContext& context, CommandsMap& commands, int lastExitCode, InputHandler& handler) {
+char run(Context& context, CommandsMap& commands, int lastExitCode, InputHandler& handler) {
     std::string inString = handler.enterString("[" + std::to_string(lastExitCode) + "] # ");
     if (inString.length() ==  0) return ERR_INPUT_EMPTY;
     std::vector<std::string> tokens = handler.splitString(inString, ' ');
@@ -199,7 +85,7 @@ bool handleExitCode(const char exitCode, int& lastExitCode, I18N& i18n) {
 int main() {
     CommandsMap commands;
     I18N i18n;
-    FileContext context;
+    Context context;
     InputHandler handler(i18n);
     int lastExitCode = 0;
 
